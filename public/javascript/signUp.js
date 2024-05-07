74c795146cb4581e56f7d0779fdaec7b1cd14e3d
Walnut <s4020232@rmit.edu.vn>
2024-05-07 21:41:27 +0700

    CDN

--- public/javascript/signUp.js
+++ public/javascript/signUp.js
@@ -25,4 +25,50 @@ showConfirmPassword.addEventListener("click", function(){
 });
 
 // validation form sign up 
+document.getElementById('form').addEventListener('submit', function(event) {
+    event.preventDefault();
 
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var confirmPassword = document.getElementById('confirm-password').value;
+
+    if(name === '') {
+        alert('Please enter your name');
+        return false;
+    }
+
+    if(email === '') {
+        alert('Please enter your email');
+        return false;
+    }
+
+    var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if(!emailRegex.test(email)) {
+        alert('Please enter a valid email');
+        return false;
+    }
+
+    if(password === '') {
+        alert('Please enter your password');
+        return false;
+    }
+
+    var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if(!passwordRegex.test(password)) {
+        alert('Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character');
+        return false;
+    }
+    if(confirmPassword === '') {
+        alert('Please confirm your password');
+        return false;
+    }
+
+    if(password !== confirmPassword) {
+        alert('Passwords do not match');
+        return false;
+    }
+
+    alert('Form is valid and ready to submit');
+    return true;
+});
