3e5ebbaef5ebdb292fa4aea41fe0ad740da16dea
Walnut <s4020232@rmit.edu.vn>
2024-05-14 11:33:54 +0700

    dead

--- public/javascript/sidebar.js
+++ public/javascript/sidebar.js
@@ -17,37 +17,4 @@ modeSwitch.addEventListener('click', () => {
     body.classList.toggle('dark');
     modeText.textContent = body.classList.contains('dark') ? 'Light Mode' : 'Dark Mode';
 });
-document.addEventListener("DOMContentLoaded", function () {
-  var dropdowns = document.querySelectorAll(".nav-link");
 
-  dropdowns.forEach(function (dropdown) {
-    dropdown.addEventListener("click", function (event) {
-      event.preventDefault();
-      var submenu = this.querySelector(".sub-menu");
-      if (this.classList.contains("active")) {
-        this.classList.remove("active");
-        submenu.style.display = "none";
-      } else {
-        closeAllDropdowns();
-        this.classList.add("active");
-        submenu.style.display = "block";
-      }
-    });
-  });
-
-  // Close all dropdowns except the clicked one
-  function closeAllDropdowns() {
-    dropdowns.forEach(function (dropdown) {
-      dropdown.classList.remove("active");
-      var submenu = dropdown.querySelector(".sub-menu");
-      submenu.style.display = "none";
-    });
-  }
-
-  // Close dropdowns when clicking outside
-  document.addEventListener("click", function (event) {
-    if (!event.target.closest(".nav-link")) {
-      closeAllDropdowns();
-    }
-  });
-});
